1c5c894369706264d7781e2cf08acc593c096ecf
Yekutiel Katanov <katanov770@gmail.com>
2022-11-04 14:39:06 +0200

    Redux intro

--- src/App.js
+++ src/App.js
@@ -5,23 +5,32 @@ import Header from './components/Header';
 import Cart from './pages/Cart';
 import Home from './pages/Home';
 import NotFound from './components/NotFoundBlock/NotFound';
+import { useSelector, useDispatch } from 'react-redux';
 import './scss/app.scss';
+import { decrement, increment } from './redux/slices/filterSlice';
 export const SearchContext = createContext('');
 
 function App() {
   const [searchValue, setSearchValue] = useState('');
+  const count = useSelector((state) => state.counter.count);
+  const dispatch = useDispatch();
 
   return (
     <div className="wrapper">
+      <button aria-label="Increment count" onClick={() => dispatch(increment())}>
+        Increment
+      </button>
+      <span>{count}</span>
+      <button aria-label="Decrement count" onClick={() => dispatch(decrement())}>
+        Decrement
+      </button>
+
       <SearchContext.Provider value={{ searchValue, setSearchValue }}>
         <Header />
         <div className="content">
           <div className="container">
             <Routes>
-              <Route
-                path="/"
-                element={<Home />}
-              />
+              <Route path="/" element={<Home />} />
               <Route path="/cart" element={<Cart />} />
               <Route path="/cart_empty" element={<NotFound />} />
             </Routes>
--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+
+import { store } from './redux/store';
+import { Provider } from 'react-redux';
+
 import './index.scss';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <BrowserRouter>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </BrowserRouter>,
+  <Provider store={store}>
+    <BrowserRouter>
+      <React.StrictMode>
+        <App />
+      </React.StrictMode>
+    </BrowserRouter>
+    ,
+  </Provider>,
 );
